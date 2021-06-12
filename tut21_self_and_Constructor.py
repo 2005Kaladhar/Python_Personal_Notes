d626ae36e3485d33910aa9fd7f4b4ee02a497037
Kaladhar Gopal <52046732+2005Kaladhar@users.noreply.github.com>
2021-06-12 22:54:11 +0530

    Add files via upload

---
+++ tut21_self_and_Constructor.py
@@ -0,0 +1,153 @@
+'''
+Undetstanding 'self' keyword and constructor '.__init__' in OOPS
+
+> Functions created inside a class are called 'methods'
+
+Creating a method of a class Employee-
+
+----------------------------------------------------------
+Note - Difference betweem parameter and argument.
+. Parameter - things which need to be passed to a function.
+. Arguments - things which are given to a function while calling it.
+----------------------------------------------------------------
+
+__________COde_____________________-
+
+class Employee:
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee()
+
+kaladhar.name = "kaladhar"
+kaladhar.role = 'student'
+kaladhar.salary = 786
+
+kaladhar.who_is_it()
+
+-----------------
+Ouput - 
+------------
+Name - kaladhar, Salary - 786 Role - student
+
+---------------------------------------
+Explanation - In the above class Employee, we created a method named
+------------
+'who_is_it', in that method, the first parameter is 'self'.
+
+As we know that we can instance methods on any instance, while 
+running any method, it is the convention that the instance will
+be given to the method as its first argument and to handle it 
+we use 'self' keyword.
+
+'self' should be the first parameter of every method of a class
+except it is a static method or a class method which will be 
+discussed later.
+
+
+Q. What is the first parameter of a method ?
+Ans - 'self' is the first parameter of a method.
+
+Q. What is the first argument given to a method ??
+Ans - The instance itself on which the method is applied
+is the first argument given to the it.
+
+
+------------------------------------------------------------------------
+-------------------------------------------------------------------------
+                    Understanding Constructor
+                    
+                    
+In the previous exmaple of Employee class, we manually created
+instance variables like 'name', 'salary', 'role' etc.
+
+What if I need such variables 'name', 'salary', 'role' etc. for 
+every instance of Employee class ??
+
+Manually creating the same variables for every instance breaks the
+rule of DRY .
+
+If we want to do a specific task at the time of creation of an instance
+of the class, we use constructors.
+
+. Constructor is a special method which is automatically run by any
+class while creating an instance(object).
+
+>> Creating a constructor and re-writing the previous code
+
+__________COde__________
+
+class Employee:
+    def __init__(self,naam,vetan,kaam):
+        # Cerating instance variables
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+    
+    
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee('kaladhar',456,'Student')
+
+kaladhar.who_is_it()
+
+# Accessing instance method with the help of class 
+Employee.who_is_it(kaladhar)
+
+----------------------------
+output - 
+Name - kaladhar, Salary - 786 Role - student
+------------------------------
+
+
+Q. Where do the arguments given to a class go ?
+Ans - All the arguments give to a class go to its constructor
+
+
+>> How to acces intance method with the name of a class ???
+Ans - As we know that when we call an instance method with its name,
+automatically the instance is given as the fist argument to the method and 
+there we don't need to specify the instance.
+
+But if we want to run an instance method with the help of a class, then
+in that case we'll have to provide it the instance.
+
+
+
+
+-----------------------------------------------------------------------
+
+
+
+
+
+'''
+class Employee:
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+    
+    
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee('kaladhar',456,'Student')
+
+kaladhar.who_is_it()
+
+# Accessing instance method with the help of class 
+Employee.who_is_it(kaladhar)
+        
+        
+
+
+
+
+
+
---
+++ tut22_Types_of_Methods.py
@@ -0,0 +1,230 @@
+'''
+================Understanding types of methods.=====================
+
+>> Types of methods
+There are three types of methods.
+
+1. Instance methods - That we generally make, the take the name of instance as their first argument
+
+2. Class methods - Which take class name as the first arugment
+
+3. Static methods - which are normal methods, they neither take class name nor instance name
+they are used to perform functions which don't need the name of 
+class or instance.
+
+
+Note - While calling an instance method with the name of the instance,
+then we don't need to specify the object as it automatically goes to
+the class.
+
+But if we want to run an instance method with the name of the class, then
+in that case we need to specify the object like this - 
+
+Employee - a class
+kaladhar - an instance
+print_details() - a method in Employee class
+
+_____COde_________
+Employee.print_details(kaladhar)
+____________________
+
+
+================= Understanding Class Methods ======================
+
+As instance methods are used to perform some operation on the instance,
+class methods are used to perform some operations on the class.
+
+We define that a method is class method with the help of a decorator
+'@classmethod'
+
+class Employee:
+    sick_leave = 10
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+        
+    @classmethod
+    def sick_changer(cls,new_leaves):
+        cls.sick_leave = new_leaves
+            
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee('kaladhar',456,'Student')
+
+print("Before running the class method ",kaladhar.sick_leave)
+
+#Changing sick_leaves from class name
+Employee.sick_changer(50)   #We could have also done this with the class name.
+
+# But the magic of class method is here.
+kaladhar.sick_changer(50)   # This is the power of classmethod
+
+print("After running the class method ",kaladhar.sick_leave)
+
+-----------------------------------
+Output-
+Before running the class method  10
+After running the class method  50
+---------------------------------
+
+Explanation - 
+In the above class, we made a class method by using '@classmethod' decorator.
+As an instance method takes the instance as the first argument, a class
+method takes the class a the first argument.
+
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+
+======== Another way of changing class variables wih instance ========
+
+.> We can access class of an instance with help of an attribute '.__class__'
+
+________COde___________
+
+class Employee:
+    sick_leave = 10
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+        
+
+    def sick_changer(self,new_leave):
+        self.__class__.sick_leave = new_leave
+    
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee('kaladhar',456,'Student')
+
+print("Before running the class method ",kaladhar.sick_leave)
+
+kaladhar.sick_changer(50)
+
+print("After running the class method ",kaladhar.sick_leave)
+
+-------------------------------
+Ouput - 
+Before running the class method  10
+After running the class method  50
+---------------------------------
+
+
+============= Class method as an alternative constructor =============
+
+.> Now we can use class method to make an alernatice constructor.
+Task - Take only one string argument in class and make all the instance variables
+from the constructor of the class.
+
+
+class Employee:
+    sick_leave = 10
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+    
+    @classmethod
+    def alter_construct(cls,constructor_string):
+        return cls(*constructor_string.split(' '))
+
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee('kaladhar',456,'Student')
+
+print("Before running the class method ",kaladhar.sick_leave)
+
+kaladhar.sick_changer(50)
+
+print("After running the class method ",kaladhar.sick_leave)
+
+_______COde_______
+class Employee:
+    sick_leave = 10
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+    
+    @classmethod
+    def alter_construct(cls,constructor_string):
+        # splitter = constructor_string.split(" ")
+        # name = splitter[0]
+        # salary = splitter[1]
+        # role = splitter[2]
+        # return cls(name,salary,role)
+        
+        #Or we can do it in a much better and cleaner way.
+        return cls(*constructor_string.split(' '))
+
+    def who_is_it(self):
+        print(f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee.alter_construct('kaladhar 456 Student')
+
+print(kaladhar.who_is_it())
+
+--------------------------------------------
+Ouput - 
+Name - kaladhar, Salary - 456 Role - Student
+---------------------------------------------
+
+Explanation - 
+
+In the above code, we create a classmethod named 'alter_constructor'
+which takes class name and a string in which all the values are separated
+by space.
+
+Inisde the classmethod, we are splitting the string with ' ' and getting 
+a list of all the values that are to be given to class to make an object.
+
+At the end we are returning the cls and passing all the arguments to it.
+Conventional way is to first split it and then pass it one by one.
+
+We can do this we the help of *args and directly passing all the values to
+the class in single shot
+
+
+
+
+'''
+class Employee:
+    sick_leave = 10
+    def __init__(self,naam,vetan,kaam):
+        # Creating instance variables.
+        self.name = naam
+        self.salary = vetan
+        self.role = kaam
+    
+    @classmethod
+    def alter_construct(cls,constructor_string):
+        # splitter = constructor_string.split(" ")
+        # name = splitter[0]
+        # salary = splitter[1]
+        # role = splitter[2]
+        # return cls(name,salary,role)
+        return cls(*constructor_string.split(' '))
+
+    def who_is_it(self):
+        return (f"Name - {self.name}, Salary - {self.salary}"\
+              f" Role - {self.role}")
+
+kaladhar = Employee.alter_construct('kaladhar 456 Student')
+
+print(kaladhar.who_is_it())
+
+
+
+
+
